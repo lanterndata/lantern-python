d79ac1056ce254f3d6445d38daf64b4afed05bce
Di Qi <di@lantern.dev>
2024-01-05 13:29:43 -0800

    Increment version

--- lantern/setup.py
+++ lantern/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setuptools.setup(
     name='lantern-client',
-    version='0.0.2',    
+    version='0.0.3',
     description='Python client for Lantern',
     url='https://github.com/lanterndata/lantern-python',
     author='Varik Matevosyan',
