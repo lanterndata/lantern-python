80f8d844462d022b2057d9cb43896a2a4ee44640
Di Qi <di@lantern.dev>
2024-01-05 22:12:29 -0800

    Add comments

--- lantern/test/test_client.py
+++ lantern/test/test_client.py
@@ -1,60 +1,78 @@
+from lantern import SyncClient
 import os
 import sys
 sys.path.append('../src')
-from lantern import SyncClient
 
 DB_URL = os.environ.get("DB_URL")
 
 if DB_URL is None:
-    raise(Exception("Please provide 'DB_URL' env variable"))
+    raise (Exception("Please provide 'DB_URL' env variable"))
+
 
 def test_client():
-    client = SyncClient(url=DB_URL, table_name="small_world", dimensions=3, distance_type="l2sq", m=12, ef=64, ef_construction=64)
+    # Initialize the SyncClient with the database URL and table configuration
+    client = SyncClient(url=DB_URL, table_name="small_world", dimensions=3,
+                        distance_type="l2sq", m=12, ef=64, ef_construction=64)
+
+    # Attempt to drop the existing table if it exists
     try:
-      client.drop()
+        client.drop()
     except:
-     pass
+        pass
+
+    # Create a new table
     client.create_table()
+
+    # Bulk insert data into the table
     client.bulk_insert([
-      ("1", [0,0,0], { "name": "a" }),
-      ("2", [0,1,0], { "name": "b" }),
-      ("3", [0,0,1], { "name": "c" })
+        ("1", [0, 0, 0], {"name": "a"}),
+        ("2", [0, 1, 0], {"name": "b"}),
+        ("3", [0, 0, 1], {"name": "c"})
     ])
+
+    # Create an index to optimize search queries
     client.create_index()
 
-# Select specific fields
+    # Retrieve a specific record by ID, selecting only certain fields
     vec_by_id = client.get_by_id(id="1", select_fields=["id", "metadata"])
-    assert(vec_by_id.embedding == None)
-# Get by id
+    assert (vec_by_id.embedding is None)
+
+    # Retrieve a record by ID without field restrictions
     vec_by_id = client.get_by_id("1")
-    assert(vec_by_id.id == "1")
+    assert (vec_by_id.id == "1")
 
-# Get by ids
+    # Retrieve multiple records by their IDs
     vectors_by_ids = client.get_by_ids(["1", "3"])
-    assert(len(vectors_by_ids) == 2)
-    assert(vectors_by_ids[0].id == "1")
-    assert(vectors_by_ids[1].id == "3")
+    assert (len(vectors_by_ids) == 2)
+    assert (vectors_by_ids[0].id == "1")
+    assert (vectors_by_ids[1].id == "3")
 
-# Insert one
-    client.upsert(("4", [1,0,0], { "name": "d" }))
+    # Insert or update a single record
+    client.upsert(("4", [1, 0, 0], {"name": "d"}))
 
-# Update
-    client.update_by_id(id="4", metadata={ "name": "4" })
+    # Update the metadata of a record by its ID
+    client.update_by_id(id="4", metadata={"name": "4"})
 
-# Get row count
+    # Get the total number of records in the table
     row_count = client.count()
-    assert(row_count == 4)
-    
-    vec_by_id = client.get_by_id(id="4", select_fields=["id", "embedding", "metadata"])
-    assert(vec_by_id.metadata.name == "4")
+    assert (row_count == 4)
 
+    # Retrieve a record by ID, selecting specific fields
+    vec_by_id = client.get_by_id(id="4", select_fields=[
+                                 "id", "embedding", "metadata"])
+    assert (vec_by_id.metadata['name'] == "4")
+
+    # Search for vectors similar to the one with ID "4"
     vectors = client.search(query_id="4")
-    assert(len(vectors) == row_count)
-    assert(vectors[0].id == "4")
-    assert(vectors[0].distance == 0)
-    vectors = client.search(query_embedding=vec_by_id.embedding, limit=2, filter={"name": "a"}, select_fields=["id"])
-    assert(len(vectors) == 1)
-    assert(vectors[0].id == "1")
-    assert(vectors[0].metadata == None)
-    assert(vectors[0].embedding == None)
+    assert (len(vectors) == row_count)
+    assert (vectors[0].id == "4")
+    assert (vectors[0].distance == 0)
 
+    # Search for vectors using the embedding of the vector with ID "4"
+    # Limit the results to 2 and apply a filter on the metadata
+    vectors = client.search(query_embedding=vec_by_id.embedding, limit=2, filter={
+                            "name": "a"}, select_fields=["id"])
+    assert (len(vectors) == 1)
+    assert (vectors[0].id == "1")
+    assert (vectors[0].metadata is None)
+    assert (vectors[0].embedding is None)
