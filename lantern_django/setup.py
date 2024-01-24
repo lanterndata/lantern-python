ebfaa515c9604a9db9b203221e9859bb55fe2f4b
Di Qi <di@lantern.dev>
2024-01-23 23:36:50 -0800

    Add setup.py

---
+++ lantern_django/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='lantern-django',
+    version='0.0.0',
+    description='Django client for Lantern',
+    url='https://github.com/lanterndata/lantern-python',
+    author='Di Qi',
+    author_email='di@lantern.dev',
+    license='BSL 1.1',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    project_urls={
+        "Bug Tracker": "https://github.com/lanterndata/lantern-python/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"lantern_django": "src"},
+    python_requires=">=3.8",
+    install_requires=[
+        'django',
+        'numpy'
+    ]
+)
